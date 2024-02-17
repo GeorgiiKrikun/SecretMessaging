a11ff9a0d853feba4db864eb19dddb4521dc621d
Georgii <georgii.krikun@gmail.com>
2024-02-17 16:17:32 +0100

    Fixes for telegram and gchat messengers

--- home/secret_messaging/InvisibleSymbolBinary.py
+++ home/secret_messaging/InvisibleSymbolBinary.py
@@ -1,26 +1,30 @@
-i1 = "\u200B"
-i2 = "\u200C"
-i_delimiter = "\u200D"
+
+i_0 = "\u200C"
+i_1 = "\u200D"
 
 import base64
 
 def int_to_invisible_string(i: int) -> str:
     # Convert integer to binary reprsentation
     binary = bin(i)[2:]
+    # Append 0s to make the length a multiple of 8
+    while len(binary) % 8 != 0:
+        binary = "0" + binary
     # Convert binary to invisible string
     result = ""
+
     for bit in binary:
         if bit == "0":
-            result += i1
+            result += i_0
         else:
-            result += i2
+            result += i_1
     return result
 
 def int_from_invisible_string(s: str) -> int:
     # Convert invisible string to binary
     binary = ""
     for char in s:
-        if char == i1:
+        if char == i_0:
             binary += "0"
         else:
             binary += "1"
@@ -33,14 +37,13 @@ def make_string_invisible(s: str) -> str:
     s_enc = s_b64.encode()
     for byte in s_enc:
         result += int_to_invisible_string(byte)
-        result += i_delimiter
 
     return result
 
 def make_string_visible(s: str) -> str:
     result = ""
-    s = [x for x in s.split(i_delimiter) if x != '']
-    for byte in s:
+    for i in range(0, len(s), 8):
+        byte = s[i:i+8]
         result += chr(int_from_invisible_string(byte))
     result = base64.b64decode(result).decode()
     return result
@@ -48,7 +51,7 @@ def make_string_visible(s: str) -> str:
 def extract_important_symbols(s: str) -> str:
     result = ""
     for char in s:
-        if char == i1 or char == i2 or char == i_delimiter:
+        if char == i_0 or char == i_1:
             result += char
     return result
 
