84a472d0959ee8ef1e195397c2d21de3461c74d0
Georgii <georgii.krikun@gmail.com>
2024-02-17 21:21:29 +0100

    Make better css

--- home/secret_messaging/urls.py
+++ home/secret_messaging/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.create_secret, name='index'),
     path('hide_secret/', views.create_secret, name='secret_string'),
     path('reveal_secret/', views.reveal_secret, name='reveal_secret'),
 ]
