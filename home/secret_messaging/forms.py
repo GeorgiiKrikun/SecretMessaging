e021e111605d65c5f32cf968a3bd2b5fedfe734e
Georgii <georgii.krikun@gmail.com>
2024-02-29 22:14:01 +0100

    Add new css style to main page

--- home/secret_messaging/forms.py
+++ home/secret_messaging/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class create_secret_form(forms.Form):
-    message = forms.CharField(label='message', max_length=200)
-    secret = forms.CharField(label='secret', max_length=200)
+    message = forms.CharField(label='message', max_length=200, widget=forms.TextInput(attrs={'class': 'form-input'}))
+    secret = forms.CharField(label='secret', max_length=200, widget=forms.TextInput(attrs={'class': 'form-input'}))
 
 class reveal_secret_form(forms.Form):
     secret_string = forms.CharField(label='secret string')
