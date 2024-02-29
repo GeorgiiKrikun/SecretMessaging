c0956b7b54c51cd4786e63d4a545ccb2c36dfdb3
Georgii <georgii.krikun@gmail.com>
2024-02-29 22:31:41 +0100

    Further refactoring

--- home/secret_messaging/forms.py
+++ home/secret_messaging/forms.py
@@ -5,4 +5,4 @@ class create_secret_form(forms.Form):
     secret = forms.CharField(label='secret', max_length=200, widget=forms.TextInput(attrs={'class': 'form-input'}))
 
 class reveal_secret_form(forms.Form):
-    secret_string = forms.CharField(label='secret string')
+    secret_string = forms.CharField(label='secret string',max_length=200, widget=forms.TextInput(attrs={'class': 'form-input'}))
