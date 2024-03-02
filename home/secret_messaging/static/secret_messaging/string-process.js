b7b757abd97e061aca2843ac69a71bd4f0ca8672
Georgii <georgii.krikun@gmail.com>
2024-03-02 20:47:47 +0100

    Add wasm client side encription

--- home/secret_messaging/static/secret_messaging/string-process.js
+++ home/secret_messaging/static/secret_messaging/string-process.js
@@ -13,3 +13,21 @@ function reveal_secret(message) {
     }
     return bindings.extract_secret(message);
 }
+
+function create_secret_button_pressed() {
+    var message = document.getElementById('id_message').value;
+    var secret = document.getElementById('id_secret').value;
+    var result = create_secret(message, secret);
+    document.getElementById('id_secret_string').value = result;
+    var request_div = document.getElementById('id_request');
+    var result_div = document.getElementById('id_result');
+    request_div.style.display = 'none';
+    result_div.style.display = 'inline-flex';
+}
+
+function go_back() {
+    var request_div = document.getElementById('id_request');
+    var result_div = document.getElementById('id_result');
+    request_div.style.display = 'inline-flex';
+    result_div.style.display = 'none';
+}
