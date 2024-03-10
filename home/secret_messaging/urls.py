13a7950f645b102c07cf121745ad8184bd5e445e
Georgii <georgii.krikun@gmail.com>
2024-03-10 21:55:22 +0100

    Add redirect to the main page

--- home/secret_messaging/urls.py
+++ home/secret_messaging/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.create_secret, name='index'),
     path('hide_secret/', views.create_secret, name='secret_string'),
     path('reveal_secret/', views.reveal_secret, name='reveal_secret'),
+    re_path(r'^.*$', views.redirect_view, name='redirect_view'),
+
 ]
--- home/secret_messaging/views.py
+++ home/secret_messaging/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+def redirect_view(request):
+    response = redirect('secret_string')
+    return response
 
 def create_secret(request):
     return render(request, 'secret_messaging/create_secret_string.html')
